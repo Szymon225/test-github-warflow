eb19e00e220dd26520f509754383c02337079ca9
Szymon225 <114868549+Szymon225@users.noreply.github.com>
2023-05-06 14:06:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+"""Import"""
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
+    """Module printing text"""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
     app.run(debug=True)
